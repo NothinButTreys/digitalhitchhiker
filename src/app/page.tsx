4969db7b8ffb3ddf6c23c2786f4b23deb50fd367
Eddie Sanmarco <ehsanmarco@gmail.com>
2023-08-12 14:41:26 -0700

    cleaned up directory structure; added length check to server call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,61 @@
-import React from "react";
-import "animate.css";
-import Loading from "./loading";
-import getPhotos from "./fetchPhotos";
-import {Photo} from "./types";
-import ImageGallery from "./ImageGallery";
-
+import getPhotos, { type Photo } from '@/server/photos';
+import Image from 'next/image';
 
 export default async function Home() {
-  const results: Photo[] = await getPhotos();
+	const photos = await getPhotos();
 
-  if (!results || results.length === 0) {
-    return <Loading />;
-  }
+	const sortPhotosByFolder = (photos: Photo[]) => {
+		let sortedPhotos = {} as { [key: string]: Photo[] };
+		for (let photo of photos) {
+			if (!sortedPhotos[photo.folder]) {
+				sortedPhotos[photo.folder] = [];
+			}
+			sortedPhotos[photo.folder].push(photo);
+		}
 
-  const sortPhotosByFolder = (photos: Photo[]): { [key: string]: Photo[] } => {
-    let sortedPhotos: { [key: string]: Photo[] } = {};
-    for (let photo of photos) {
-      if (!sortedPhotos[photo.folder]) {
-        sortedPhotos[photo.folder] = [];
-      }
-      sortedPhotos[photo.folder].push(photo);
-    }
+		return sortedPhotos;
+	};
 
-    return sortedPhotos;
-  };
+	return (
+		<div
+			className="relative p-4 top-20 animate__animated animate__fadeIn"
+			id="imageGallery"
+		>
+			{Object.entries(sortPhotosByFolder(photos)).map(
+				([folderName, photos]) => (
+					<ImageGallery
+						key={folderName}
+						folderName={folderName}
+						photos={photos}
+					/>
+				)
+			)}
+		</div>
+	);
+}
 
-  
-
-  return (
-    <div className="p-4 top-20 relative animate__animated animate__fadeIn" id="imageGallery">
-      {Object.entries(sortPhotosByFolder(results)).map(
-        ([folderName, photos]) => (
-          <ImageGallery
-            key={folderName}
-            folderName={folderName}
-            photos={photos}
-          />
-        )
-      )}
-    </div>
-  );
+function ImageGallery({
+	folderName,
+	photos,
+}: {
+	folderName: string;
+	photos: Photo[];
+}) {
+	return (
+		<div className="relative gap-8 columns-2 md:columns-4">
+			{photos.map((photo) => (
+				<Image
+					key={photo.public_id + folderName}
+					src={photo.secure_url}
+					alt={photo.public_id}
+					className="h-auto max-w-full py-4 rounded-lg"
+					width={625}
+					height={444}
+				/>
+			))}
+			<div className="absolute px-2 py-4 shadow-md top-4 left-4 bg-black/25 rounded-2xl">
+				{folderName}
+			</div>
+		</div>
+	);
 }
